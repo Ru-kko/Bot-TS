850b8d6f12a488be01a3fbbc11e96ceb4bcf250d
Rukko <verase2003.1@gmail.com>
2021-10-18 14:05:29 -0500

    Error ypes was added

--- src/crud/crud.ts
+++ src/crud/crud.ts
@@ -2,7 +2,7 @@ import { RowDataPacket } from 'mysql2';
 import connection from './connection';
 
 const Crud = new class {
-    async putUser(userID: number | String) {
+    async putUser(userID: number | String): Promise<void> {
         try {
             const cnt = await connection();
             await cnt.query(`INSERT INTO users(usr_id) Values(${userID})`);
@@ -10,6 +10,7 @@ const Crud = new class {
         } catch (e) {
             console.log(e);
         };
+        return;
     };
     async putServer(serverID: number | String) {
         try {
@@ -17,9 +18,9 @@ const Crud = new class {
             await cnt.query(`INSERT INTO servers(sv_id) Values(${serverID})`);
             cnt.end();
         } catch (e) {
-            if((e as Error).message == `Duplicate entry '${serverID}' for key 'servers.PRIMARY'`){
+            if ((e as Error).message == `Duplicate entry '${serverID}' for key 'servers.PRIMARY'`) {
                 throw new Error('This server already exits');
-            }else{
+            } else {
                 console.log(e);
             }
         };
@@ -27,7 +28,12 @@ const Crud = new class {
     async userJoin(serverID: string | number, userID: string | number) {
         try {
             const cnt = await connection();
-            await cnt.query(`INSERT INTO usrs_srvs(usr_id, sv_id) Values(${userID}, ${serverID})`);
+            await cnt.query(`INSERT INTO usrs_srvs(usr_id, sv_id) Values(${userID}, ${serverID})`).catch(async (e) => {
+                if (e.code == 'ER_NO_REFERENCED_ROW_2'){
+                    await this.putUser(userID);
+                    this.userJoin(serverID, userID);
+                }
+            });
             cnt.end();
         } catch (e) {
             console.log(e);
@@ -42,68 +48,81 @@ const Crud = new class {
      * -1 whether the user will level up
      * -2 whether the query has an error
      */
-    async addXP(serverID: string | number, userID: string | number, xp: number):Promise<number>{
-        var res:number;
-        try{
+    async addXP(serverID: string | number, userID: string | number, xp: number): Promise<number> {
+        var res: number;
+        try {
             const where = `WHERE usr_id = ${userID} AND sv_id = ${serverID}`;
 
             const cnt = await connection();
-            const [response,_] = await cnt.query<RowDataPacket[]>(`SELECT sv_t_xp,act_level FROM usrs_srvs ${where}`);
-            
-            if(response[0] == undefined){ cnt.end() ;throw `The user id ${userID} not exits in server ${serverID}`};
-            
-            var [total_xp,level] = [response[0]['sv_t_xp'],response[0]['act_level']];
-            
+            const [response, _] = await cnt.query<RowDataPacket[]>(`SELECT sv_t_xp,act_level FROM usrs_srvs ${where}`);
+
+            if (response[0] == undefined) { cnt.end(); throw new userNoExist(serverID, userID) };
+
+            var [total_xp, level] = [response[0]['sv_t_xp'], response[0]['act_level']];
+
 
             // method
-            const nextlevel:number = (10 * (level*level)) + Math.pow( 10 * (level + 1) , 2);
-            
+            const nextlevel: number = (10 * (level * level)) + Math.pow(10 * (level + 1), 2);
+
             total_xp += xp;
 
-            if(nextlevel > total_xp){
+            if (nextlevel > total_xp) {
                 level += 1;
                 await cnt.query(`Update usrs_srvs set sv_t_xp = ${xp} ${where}`);
                 res = 0;
-            }else{
+            } else {
                 await cnt.query(`Update usrs_srvs set sv_t_xp = ${xp}, act_level = ${level} ${where}`)
                 res = 1;
             };
 
             cnt.end();
-        }catch(e){
-            console.log(e);
+        } catch (e: userNoExist | any) {
+            if(e.type){
+                this.userJoin(serverID, userID)
+            }
             res = 2;
         };
         return res;
     };
-    async getServerPrefix(serverID: string | number): Promise<String>{
-        try{
+    async getServerPrefix(serverID: string | number): Promise<String> {
+        try {
             const cnt = await connection();
-            const [response,_] = await cnt.query<RowDataPacket[]>(`Select prefix FROM servers WHERE sv_id = ${serverID}`);
+            const [response, _] = await cnt.query<RowDataPacket[]>(`Select prefix FROM servers WHERE sv_id = ${serverID}`);
             cnt.end();
             return response[0]['prefix'];
-        }catch(e){
+        } catch (e) {
             console.log(e);
             return "error" + e;
         }
     };
-    async setServerPrefix(serverID: string | number, newPrefix: String){
-        try{
+    async setServerPrefix(serverID: string | number, newPrefix: String) {
+        try {
             const cnt = await connection();
             await cnt.query(`Update servers set prefix = '${newPrefix}' WHERE sv_id = ${serverID}`);
             cnt.end();
-        }catch(e){
+        } catch (e) {
             console.log(e);
         }
     };
-    async deleteServer(serverID: string | number){
-        try{
+    async deleteServer(serverID: string | number) {
+        try {
             const cnt = await connection();
             await cnt.query(`Delete From servers where sv_id = ${serverID}`)
-        }catch(e){
+        } catch (e) {
             console.log(e);
         }
     };
 };
+
+class userNoExist  {
+    serverID: Number | string;
+    userID: Number | string;
+    public type = "error";
+
+    constructor(server: Number | string, user: Number | string){
+        this.serverID = server;
+        this.userID = user;
+    }
+}
 
 export default Crud;
