7f7569188e189b0e8dd72791792f4b45e2d0cfc0
Rukko <verase2003.1@gmail.com>
2021-10-28 17:18:54 -0500

    Two application commands was added

--- src/bot/commands.ts
+++ src/bot/commands.ts
@@ -3,8 +3,10 @@ import { Message } from "discord.js";
 
 import animeFind from './command_handler/animeFind';
 import changePrefix from "./command_handler/changePrefix";
+import chess from "./command_handler/chess";
 import help from "./command_handler/help";
 import init from "./command_handler/init";
+import youtube from "./command_handler/youtube";
 
 registerFont('./public/MPLUS.ttf', { family: 'MPLUS' });
 
@@ -14,6 +16,8 @@ commandMap.set("anime", {method: animeFind, description: 'Find an anime wiht sam
 commandMap.set("help", {method: help, description: 'Show normal user commands'});
 commandMap.set("prefix", {method: changePrefix, description: 'Show server prefix, if you are an admin you can change them'});
 commandMap.set("init", {method: init, admin: true, description: 'If you\'re an admin'});
+commandMap.set("chess", {method: chess, description: "PJoin in voice channel and play chess wiht your friends"});
+commandMap.set("youtube", {method: youtube, description: "See youtube videos wiht your friends"});
 
 export interface commandSpesifications {
     method: (msg: Message) => Promise<void>;
