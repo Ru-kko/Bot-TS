0e43caf52780add5caef05b9da60502f98b201d3
Ru-kko <verase2003.1@gmail.com>
2021-12-27 19:56:12 -0500

    embed compnent was added

---
+++ web/src/components/embed/Embed.tsx
@@ -0,0 +1,44 @@
+import { PropsWithChildren } from "react";
+import { Avatar } from "../images/images"
+import "./embed.css"
+
+interface options {
+    color?: string,
+    image?: string,
+    title: String,
+    footer?: boolean
+}
+export default (props: PropsWithChildren<options>) => {
+    const footer = () => {
+        if (props.footer) {
+            const todayDate = new Date();
+            return (
+                <p
+                    style={{
+                        fontWeight: 600,
+                        fontSize: ".7em",
+                        margin: "0 2.5vh 5px",
+                        color: "#62656B",
+                    }}
+                >
+                    {todayDate.getMonth() + '/' + todayDate.getDay() + '/' + todayDate.getFullYear()}
+                </p>
+            );
+        }
+    }
+    return (
+        <div className='embed'>
+            <div className="embed-line" style={{ backgroundColor: props.color ?? "#57F288" }} />
+            <div className="embed-content">
+                <div className="embed-header" style={{marginTop: '10px'}}>
+                    <img src={props.image ?? Avatar} />
+                    <h4 style={{margin: '0px'}}>{props.title}</h4>
+                </div>
+                <div className="embed-content">
+                    {props.children}
+                </div>
+                {footer()}
+            </div>
+        </div>
+    )
+}
