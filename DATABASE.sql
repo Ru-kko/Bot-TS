7659bd864b541d0dbea36544aa9c8d316ffbcf83
Ru-kko <verase2003.1@gmail.com>
2022-04-13 19:50:44 -0500

    Cascade Delete method to conected tables was added

--- DATABASE.sql
+++ DATABASE.sql
@@ -34,14 +34,14 @@ CREATE Table IF NOT EXISTS svr_warns(
     kick BOOLEAN DEFAULT 0,
     mute BOOLEAN DEFAULT 0,
     mute_time INT DEFAULT 0,
-    CONSTRAINT sv_warn_fk FOREIGN KEY(sv_id) REFERENCES servers(sv_id)
+    CONSTRAINT sv_warn_fk FOREIGN KEY(sv_id) REFERENCES servers(sv_id) ON DELETE CASCADE
 );
 
 -- Playlis Form Users
 CREATE Table IF NOT EXISTS users_pl(
     usr_id BIGINT(18) NOT NULL PRIMARY KEY,
     pl_id VARCHAR(22) NOT NULL,
-    CONSTRAINT usr_pl_fk FOREIGN KEY(usr_id) REFERENCES users(usr_id)
+    CONSTRAINT usr_pl_fk FOREIGN KEY(usr_id) REFERENCES users(usr_id) ON DELETE CASCADE
 );
 
 -- Users And Servers Conection
@@ -53,8 +53,8 @@ CREATE TABLE IF NOT EXISTS usrs_srvs(
     act_level INT NOT NULL DEFAULT 0,
     warns INT NOT NULL DEFAULT 0,
     usr_money INT NOT NULL DEFAULT 0,
-    CONSTRAINT user_fk FOREIGN KEY (usr_id) REFERENCES users(usr_id),
-    CONSTRAINT srv_fk FOREIGN KEY (sv_id) REFERENCES servers(sv_id)
+    CONSTRAINT user_fk FOREIGN KEY (usr_id) REFERENCES users(usr_id) ON DELETE CASCADE,
+    CONSTRAINT srv_fk FOREIGN KEY (sv_id) REFERENCES servers(sv_id) ON DELETE CASCADE
 );
 -- Sessions Storage
 
@@ -70,7 +70,7 @@ CREATE TABLE IF NOT EXISTS sessions_storage(
 	tokenCreated TIMESTAMP NOT NULL,
 	deleteDate TIMESTAMP NOT NULL,
 	created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT user_sess_fk FOREIGN KEY (userid) REFERENCES users(usr_id)
+    CONSTRAINT user_sess_fk FOREIGN KEY (userid) REFERENCES users(usr_id) ON DELETE CASCADE
 );
 
 CREATE EVENT AutoDeleteSessions
