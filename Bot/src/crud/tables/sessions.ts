03638e67b3fcf20c0becf69ca5217d90bed9833c
Ru-kko <verase2003.1@gmail.com>
2022-04-24 23:43:17 -0500

    an error in sessions database when create a session was fixed

--- Bot/src/crud/tables/sessions.ts
+++ Bot/src/crud/tables/sessions.ts
@@ -2,18 +2,19 @@ import type { SessionData } from "express-session";
 import type { RowDataPacket } from "mysql2";
 import { connection } from "../connection";
 import type { session } from "../../../../types/crud.d";
+import { Users } from "./users";
 
 class SessionsDataBase extends connection {
     async has(sid: String): Promise<Boolean> {
-        const [data, _] = await super.cnt.query<RowDataPacket[]>(
+        const [data, _] = await this.cnt.query<RowDataPacket[]>(
             `SELECT COUNT(1) as exist FROM sessions_storage WHERE ssid = '${sid}'`
         );
 
-        return !(!data[0]["exist"]);
+        return data[0]["exist"] > 0;
     }
 
     async get(sid: string): Promise<SessionData | undefined> {
-        const [data, _] = await super.cnt.query<SessionResponse[]>(
+        const [data, _] = await this.cnt.query<SessionResponse[]>(
             `SELECT * FROM sessions_storage WHERE ssid = '${sid}'`
         );
         const info = data[0];
@@ -38,20 +39,32 @@ class SessionsDataBase extends connection {
     }
 
     async set(sid: string, session: SessionData): Promise<void> {
-        await super.cnt.query(
+        const query =
             "INSERT INTO sessions_storage" +
-                "(ssid, code, originalMaxAge, refreshToken, deleteDate, token, tokenType, tokenCreated, tokenExpires, userid" +
-                "VALUES(" +
-                `'${sid}', '${session.code}', ${session.cookie.originalMaxAge}, '${session.token.refresh}',` +
-                `'${new Date(session.cookie.expires!).toISOString()}', '${
-                    session.token.token
-                }', '${session.token.type}',` +
-                `'${session.token.create}', ${session.token.expires}, ${session.userid}`
-        );
+            "(ssid, code, originalMaxAge, refreshToken, deleteDate, token, tokenType, tokenCreated, tokenExpires, userid) " +
+            "VALUES(" +
+            `'${sid}', '${session.code}', ${session.cookie.originalMaxAge} , '${session.token.refresh}', ` +
+            `'${new Date(session.cookie.expires!).toISOString().slice(0,-5)}', ` +
+            `'${session.token.token}', '${session.token.type}', ` +
+            `'${session.token.create}', ${session.token.expires}, ${session.userid})`;
+
+        await this.cnt.query(query).catch(async e => {
+            switch (e.errno) {
+                case 1452:
+                    const userManager = new Users(this);
+                    await userManager.putUser(session.userid);
+                    await this.set(sid, session)
+                    break;
+                case 1062:
+                    break; // Do noting: Duplicate entry
+                default:
+                    throw e;
+            }
+        })
     }
 
     async delete(sid: string): Promise<void> {
-        await super.cnt.query(
+        await this.cnt.query(
             `DELETE FROM sessions_storage WHERE ssid = '${sid}'`
         );
     }
@@ -67,20 +80,22 @@ class SessionsDataBase extends connection {
             tokenExpires: session.token.expires,
             tokenCreated: session.token.create,
         };
-		Object.entries(formatt).forEach(([key, val])=> {
-			if (!val) return;
-			
-			if (val instanceof Date) {
-				data.push(`${key}, '${val.toISOString()}'`)
-			} else if (key === "tokenExpires") {
-				data.push(`${key} = ${Date.now + val}`)
-			} else if (val instanceof String) {
-				data.push(`${key} = '${val}'`)	
-			}
-    	});
-		
-		await super.cnt.query(`UPDATE sessions_storage SET ${data.join()} WHERE ssid = '${sid}'`);
-	}
+        Object.entries(formatt).forEach(([key, val]) => {
+            if (!val) return;
+
+            if (val instanceof Date) {
+                data.push(`${key} = '${val.toISOString().slice(0,-5)}'`);
+            } else if (key === "tokenExpires") {
+                data.push(`${key} = ${Date.now() + val}`);
+            } else if (val instanceof String) {
+                data.push(`${key} = '${val}'`);
+            }
+        });
+
+        await this.cnt.query(
+            `UPDATE sessions_storage SET ${data.join()} WHERE ssid = '${sid}'`
+        );
+    }
 }
 
 interface SessionResponse extends RowDataPacket, session {}
