ba3b307f8636ae1dbde840a49472fd42d4e0c42f
Ru-kko <verase2003.1@gmail.com>
2022-05-01 20:03:35 -0500

    server values added

--- web/src/Config/values.config.ts
+++ web/src/Config/values.config.ts
@@ -1,6 +1,17 @@
 export default {
+    Server: {
+        url: 'http://localhost:8080',
+        endPoits: {
+            logIn: "/auth/discord"
+        }
+    },
     Discord: {
-        Auth: 'https://discord.com/api/oauth2/authorize?client_id=879945146869899294&redirect_uri=localhost%3A3000&response_type=code&scope=identify%20guilds',
+        clientID: '879945146869899294',
+        Auth: 'https://discord.com/api/oauth2/authorize?client_id=879945146869899294&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fdiscord&response_type=code&scope=identify%20email%20guilds%20guilds.join%20guilds.members.read',
         Invite: 'https://discord.com/api/oauth2/authorize?client_id=879945146869899294&permissions=8&scope=bot'
+    },
+    Links: {
+        homePath: '/',
+        loginPath: '/login'
     }
 }
